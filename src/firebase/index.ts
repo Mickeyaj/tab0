d4a3230f1740ca67122b392ad9837aa22cbc1dcc
Mickeyaj <mikaylaja28@gmail.com>
2025-12-11 19:21:30 -0800

    Initialized firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoZFDZ5ej0NmOWlzVK-Y2rMfcH-kY5ToU",
+  authDomain: "tab0-1e6b5.firebaseapp.com",
+  projectId: "tab0-1e6b5",
+  storageBucket: "tab0-1e6b5.firebasestorage.app",
+  messagingSenderId: "113497991910",
+  appId: "1:113497991910:web:77cf64897a5a7eb8bb2927"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
