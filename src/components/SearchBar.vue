2054232d71a15b64a213ae0423574102e97bc88d
Mickeyaj <mikaylaja28@gmail.com>
2025-12-13 11:59:52 -0800

    Added search bar to home view linked to google

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full max-w-xl">
+        <input
+        v-model="searchQuery"
+        @keydown.enter="search"
+        type="text"
+        placeholder="Search Google..."
+        class="w-full px-6 py-3 bg-gray-800 text-white rounded-full border border-gray-700 focus:outline-none focus:border-blue-500 text-lg"
+        >
+        </input>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    const searchQuery = ref('')
+
+    function search() {
+        if (searchQuery.value.trim()) {
+            const query = encodeURIComponent(searchQuery.value.trim())
+            window.location.href = `https://www.google.com/search?q=${query}`
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
     <div class="min-h-screen bg-gray-900 text-white p-4">
         <div class="flex flex-col items-center justify-center min-h-screen">
             <Greeting />
+            <div class="mt-8">
+                <SearchBar />
+            </div>
         </div>
         <button @click="logout" class="absolute top-4 right-4 px-4 py-2 bg-red-500 rounded">
             Sign out
@@ -14,6 +17,7 @@
     import { useRouter } from 'vue-router'
     import { useUserStore } from '@/stores/user'
     import Greeting from '@/components/Greeting.vue'
+    import SearchBar from '@/components/SearchBar.vue'
 
     const router = useRouter()
     const userStore = useUserStore()
